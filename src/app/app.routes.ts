2deaf699dbb59e79dce70fb943b2435c5ca4d05d
DanielFranceschi <danielhfmarques@gmail.com>
2024-05-01 22:20:05 -0300

    Commit 3
    Sem o poke.service.spec.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,7 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { RotasComponent } from './components/rotas/rotas.component';
 import { PokeListComponent } from './components/pokelist/pokelist.component';
 import { IbgeListComponent } from './components/ibge.components/ibge.components.component';
 export const routes: Routes = [
- { path: '', component: HomeComponent},
- { path: 'pokemons', component:  PokeListComponent},
+ { path: '', component:  PokeListComponent},
  { path: 'ibge', component: IbgeListComponent },
- { path: '**', component: HomeComponent }
 ];
